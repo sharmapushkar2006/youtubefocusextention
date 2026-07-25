75969db5ff8910f05805321effb3f038f999a33c
Pushkar Sharma <258246915+sharmapushkar2006@users.noreply.github.com>
2026-07-26 01:05:26 +0530

    added javascript file

--- popup.js
+++ popup.js
@@ -1,1 +1,44 @@
 // popup window ka code
+const focus=document.getElementById("focus");
+
+const shorts=document.getElementById("shorts");
+
+const recommendations=document.getElementById("recommendations");
+
+const instagram=document.getElementById("instagram");
+
+const reddit=document.getElementById("reddit");
+
+chrome.storage.sync.get(null,data=>{
+
+focus.checked=data.focusMode;
+
+shorts.checked=data.blockShorts;
+
+recommendations.checked=data.hideRecommendations;
+
+instagram.checked=data.blockInstagram;
+
+reddit.checked=data.blockReddit;
+
+});
+
+document.getElementById("save").onclick=()=>{
+
+chrome.storage.sync.set({
+
+focusMode:focus.checked,
+
+blockShorts:shorts.checked,
+
+hideRecommendations:recommendations.checked,
+
+blockInstagram:instagram.checked,
+
+blockReddit:reddit.checked
+
+});
+
+window.close();
+
+};
